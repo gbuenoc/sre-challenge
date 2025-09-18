e2ed801352106d1927df253cd2650a8be909612a
guilherme.bueno <guiibc94@gmail.com>
2025-09-18 19:45:15 -0300

    Ajuste codigo TF

---
+++ infra/terraform/main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source          = "./modules/vpc"
+  environment     = var.environment
+  vpc_cidr        = var.vpc_cidr
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+  azs             = var.azs
+  pods_cidr       = var.pods_cidr
+}
+
+module "eks" {
+  source             = "./modules/eks"
+  environment        = var.environment
+  kubernetes_version = var.kubernetes_version
+  vpc_id             = var.vpc_id
+  private_subnet_ids = var.private_subnet_ids
+}
+
+module "ecr" {
+  source       = "./modules/ecr"
+  environment  = var.environment
+  repositories = var.repositories
+}
+
+module "addons" {
+  source      = "./modules/addons"
+  environment = var.environment
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,67 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "pods_cidr" {
+  type = string
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version to use for the EKS cluster"
+  type        = string
+}
+
+variable "addons" {
+  description = "EKS addons to enable during cluster creation"
+  type        = map(any)
+  default     = {}
+}
+
+variable "endpoint_public_access" {
+  description = "Whether the EKS cluster API endpoint should be publicly accessible"
+  type        = bool
+  default     = false
+}
+
+variable "enable_cluster_creator_admin_permissions" {
+  description = "Grants admin permissions to the IAM identity that creates the cluster"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the EKS cluster will be deployed"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for EKS control plane and workloads"
+  type        = list(string)
+}
+
+variable "repositories" {
+  description = "Lista de nomes de repositórios ECR"
+  type        = list(string)
+}
